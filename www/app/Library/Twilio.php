56aeac4845ba878ce6c0cdfb6079c06a5771a6ae
Siebren Kranenburg <info@siebrenkranenburg.nl>
2016-07-06 10:45:19 +0200

    Fixed bugs

--- www/app/Library/Twilio.php
+++ www/app/Library/Twilio.php
@@ -42,43 +42,43 @@ class Twilio {
 
 		$locations = [
 			[
-				['lat'] => '10.88515',
-				['lng'] => '-0.75446',
+				'lat' => '10.88515',
+				'lng' => '-0.75446',
 			],
 			[
-				['lat'] => '9.268018',
-				['lng'] => '-0.871342',
+				'lat' => '9.268018',
+				'lng' => '-0.871342',
 			],
 			[
-				['lat'] => '9.300931',
-				['lng'] => '-0.5784113',
+				'lat' => '9.300931',
+				'lng' => '-0.5784113',
 			],
 			[
-				['lat'] => '10.84835',
-				['lng'] => '-0.85098',
+				'lat' => '10.84835',
+				'lng' => '-0.85098',
 			],
 			[
-				['lat'] => '10.88339',
-				['lng'] => '-0.726315',
+				'lat' => '10.88339',
+				'lng' => '-0.726315',
 			],
 			[
-				['lat'] => '9.33635',
-				['lng'] => '-1.071362',
+				'lat' => '9.33635',
+				'lng' => '-1.071362',
 			],
 			[
-				['lat'] => '11.0172',
-				['lng'] => '-2.805917',
+				'lat' => '11.0172',
+				'lng' => '-2.805917',
 			],
 			[
-				['lat'] => '10.979426',
-				['lng'] => '-2.79559',
+				'lat' => '10.979426',
+				'lng' => '-2.79559',
 			]
 		];
 
 
 		foreach($locations as $geo) {
-			$temp = (new Foreca())->getTemp($geo->lat, $geo->lng);
-			$rain = (new Foreca())->getRain($geo->lat, $geo->lng);
+			$temp = (new Foreca())->getTemp($geo['lat'], $geo['lng']);
+			$rain = (new Foreca())->getRain($geo['lat'], $geo['lng']);
 
 			$object = new Twilio();
 			$object->date    = \DateTime::createFromFormat('Y-m-d', $temp['dt'], new \DateTimeZone("Europe/Amsterdam"));
@@ -87,8 +87,8 @@ class Twilio {
 			$object->rainMM  = number_format((float) $rain['pr'], 1);
 			$object->content = $object->textFormatSpecial();
 
-			$object->_send($phoneOne)
-			$object->_send($phoneTwo)
+			$object->_send($phoneOne);
+			$object->_send($phoneTwo);
 		}
 	}
 
@@ -139,28 +139,29 @@ class Twilio {
 
 	public function textFormatOne() {
 		$str  = $this->city . ", " . $this->date->format('d-m-Y') . "\r\n";
-		$str .= "Temperature min: " . $this->tempMin . "℃ \r\n";
-		$str .= "Temperature max: " . $this->tempMax . "℃ \r\n";
-		$str .= "Expected rainfall: " . $this->rainMM;
+		$str .= "Temp min: " . $this->tempMin . "℃ \r\n";
+		$str .= "Temp max: " . $this->tempMax . "℃ \r\n";
+		$str .= "Rainfall: " . $this->rainMM;
 		return $str;
 	}
 
 	public function textFormatTwo() {
 		$str  = $this->date->format('d-m-Y') . ", " . $this->city . "\r\n";
-		$str .= "Expected rainfall: " . $this->rainMM . "\r\n";
-		$str .= "Temperature min: " . $this->tempMin . "℃ \r\n";
-		$str .= "Temperature max: " . $this->tempMax . "℃";
+		$str .= "Rainfall: " . $this->rainMM . "\r\n";
+		$str .= "Temp min: " . $this->tempMin . "℃ \r\n";
+		$str .= "Temp max: " . $this->tempMax . "℃";
 		return $str;
 	}
 
 	public function textFormatSpecial() {
 		$str  = $this->date->format('d-m-Y') . "\r\n";
-		$str .= "Expected rainfall: " . $this->rainMM . "\r\n";
-		$str .= "Temperature min: " . $this->tempMin . "℃ \r\n";
-		$str .= "Temperature max: " . $this->tempMax . "℃";
+		$str .= "Temp min: " . $this->tempMin . "℃ \r\n";
+		$str .= "Temp max: " . $this->tempMax . "℃" . "\r\n";
+		$str .= "Rainfall: " . $this->rainMM;
 		return $str;
 	}
-	protected function _send($to) {
+
+	public function _send($to) {
 		$twilio  = new \Services_Twilio($this->_sId, $this->_token);
 
 		if (!is_null($this->content)) {
